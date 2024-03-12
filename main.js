0825579260939607d751ca459a9b76961e551233
wveimar <wveimar.mamian@gmail.com>
2024-03-12 17:08:02 -0500

    wp:prueba de git page si soporta javascript traduccion

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const idiomaActual = document.getElementById('idioma');
 const listaIdiomas = document.getElementById('idiomas');
 const idiomas = document.getElementsByClassName('opcion');
 
-// Info
-const titulo = document.getElementById('titulo-info');
-const parrafo = document.getElementById('parrafo-info');
+
 
 // Toggle lista idiomas
 idiomaActual.addEventListener('click',()=>{
@@ -12,6 +10,8 @@ idiomaActual.addEventListener('click',()=>{
 });
 
 const opcionesArray = Array.from(idiomas);
+const textsToChange = document.querySelectorAll('[data-section]');
+
 
 opcionesArray.forEach((opcion)=>{
     opcion.addEventListener('click',()=>{
@@ -22,21 +22,33 @@ opcionesArray.forEach((opcion)=>{
 
 function establecerIdioma(idioma) {
     idiomaActual.getElementsByTagName('img')[0].src = `banderas/${idioma}.svg`;
-    debugger
     switch (idioma) {
         
         case 'usa':
-            titulo.textContent = 'CapiDeveloper';
-            parrafo.textContent = 'I have worked on comprehensive technology-based solutions for clinics and hospitals, integrating both management software and the work teams of each institution, achieving technological implementations that stand the test of time.Throughout my career, I have gained valuable experience in accounting and knowledge of administrative processes, thanks to my participation in various implementation projects. This experience has allowed me to understand the importance of financial and administrative management for the success of any project. Currently, I work as a front-end developer, mainly with React and Angular, actively participating in the software development cycle.    At the moment, I work as a Front-end Developer with a focus on React, AWS, Angular JS, NestJS, and NextJS.'
+                changeLanguage('en');
+                console.log('en');
             break;
         case 'latino':
-            titulo.textContent = 'CapiDeveloper';
-            parrafo.textContent = 'He trabajado en soluciones integrales de base tecnológica para clínicas y hospitales integrando tanto el software de manejo como el equipo de trabajo de cada institución, logrando implementaciones tecnológicas que permanecen en el tiempo.A lo largo de mi carrera, he acumulado una valiosa experiencia en contabilidad y conocimientos de procesos administrativos, gracias a mi participación en diversos proyectos de implementación. Esta experiencia me ha permitido comprender la importancia de la gestión financiera y administrativa para el éxito de cualquier proyecto.Actualmente me desempeño como frontend principalmente con React y angular participando activamente en el ciclo de desarrollo de software.'
+            changeLanguage('es');
+            console.log('es');
             break;
         default:
             break;
     }
 }
+
+const changeLanguage = async language => {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+  
+    for (const textToChange of textsToChange) {
+      const section = textToChange.dataset.section;
+      const value = textToChange.dataset.value;
+      textToChange.textContent = texts[section][value];
+    }
+  };
+
+
 
 document.addEventListener('DOMContentLoaded',()=>{
     switch (navigator.language) {
